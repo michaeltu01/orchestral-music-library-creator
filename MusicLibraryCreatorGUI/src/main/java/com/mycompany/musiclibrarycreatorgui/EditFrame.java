ed5dbffda112d1888bda2cd082beeb2afbe00662
michaeltu01 <michaeltu705@gmail.com>
2022-01-23 14:24:07 -0500

    Prepared EditFrame for testing

--- MusicLibraryCreatorGUI/src/main/java/com/mycompany/musiclibrarycreatorgui/EditFrame.java
+++ MusicLibraryCreatorGUI/src/main/java/com/mycompany/musiclibrarycreatorgui/EditFrame.java
@@ -4,7 +4,7 @@
  */
 package com.mycompany.musiclibrarycreatorgui;
 
-import src.backend.*;
+import src.backend;
 
 import java.awt.event.*;
 import javax.swing.*;
@@ -15,21 +15,22 @@ import javax.swing.*;
 public class EditFrame extends JPanel 
 {
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private JButton addButton;
-    private JTextField arrangerField;
-    private JButton backButton;
-    private JTextField composerField;
-    private JLabel directions;
-    private JLabel titleLabel;
-    private JLabel composerLabel;
-    private JLabel arrangerLabel;
-    private JLabel publisherLabel;
-    private JLabel vbodaGradeLabel;
-    private JLabel notesLabel;
-    private JTextField notesField;
-    private JTextField publisherField;
-    private JTextField titleField;
-    private JTextField vbodaGradeField;
+    private javax.swing.JButton addButton;
+    private javax.swing.JTextField arrangerField;
+    private javax.swing.JButton backButton;
+    private javax.swing.JTextField composerField;
+    private javax.swing.JLabel directions;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JTextField notesField;
+    private javax.swing.JTextField publisherField;
+    private javax.swing.JTextField titleField;
+    private javax.swing.JTextField vbodaGradeField;
     // End of variables declaration//GEN-END:variables
 
     /**
@@ -50,133 +51,136 @@ public class EditFrame extends JPanel
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        directions = new JLabel();
-        titleLabel = new JLabel();
-        titleField = new JTextField();
-        composerLabel = new JLabel();
-        composerField = new JTextField();
-        arrangerLabel = new JLabel();
-        arrangerField = new JTextField();
-        publisherLabel = new JLabel();
-        publisherField = new JTextField();
-        vbodaGradeField = new JTextField();
-        vbodaGradeLabel = new JLabel();
-        notesField = new JTextField();
-        notesLabel = new JLabel();
-        addButton = new JButton();
-        backButton = new JButton();
+        jLabel2 = new javax.swing.JLabel();
+        directions = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        titleField = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        composerField = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        arrangerField = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        publisherField = new javax.swing.JTextField();
+        vbodaGradeField = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        notesField = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        addButton = new javax.swing.JButton();
+        backButton = new javax.swing.JButton();
 
-        directions.setHorizontalAlignment(SwingConstants.CENTER);
+        jLabel2.setText("jLabel2");
+
+        directions.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         directions.setText("Enter the information below to add a composition.");
 
-        titleLabel.setText("Title:");
+        jLabel3.setText("Title:");
 
         titleField.setText("jTextField1");
-        titleField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
+        titleField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 titleFieldActionPerformed(evt);
             }
         });
 
-        composerLabel.setText("Composer:");
+        jLabel4.setText("Composer:");
 
         composerField.setText("jTextField1");
-        composerField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
+        composerField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 composerFieldActionPerformed(evt);
             }
         });
 
-        arrangerLabel.setText("Arranger: ");
+        jLabel5.setText("Arranger: ");
 
         arrangerField.setText("jTextField1");
-        arrangerField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
+        arrangerField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 arrangerFieldActionPerformed(evt);
             }
         });
 
-        publisherLabel.setText("Publisher:");
+        jLabel6.setText("Publisher:");
 
         publisherField.setText("jTextField1");
-        publisherField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
+        publisherField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 publisherFieldActionPerformed(evt);
             }
         });
 
         vbodaGradeField.setText("jTextField1");
-        vbodaGradeField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
+        vbodaGradeField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 vbodaGradeFieldActionPerformed(evt);
             }
         });
 
-        vbodaGradeLabel.setText("VBODA Grade: ");
+        jLabel7.setText("VBODA Grade: ");
 
         notesField.setText("jTextField1");
-        notesField.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
+        notesField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 notesFieldActionPerformed(evt);
             }
         });
 
-        notesLabel.setText("Notes:");
+        jLabel8.setText("Notes:");
 
         addButton.setText("Add");
-        addButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
+        addButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addButtonActionPerformed(evt);
             }
         });
 
         backButton.setText("Back");
 
-        GroupLayout layout = new GroupLayout(this);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(89, 89, 89)
                                 .addComponent(directions))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(24, 24, 24)
-                                .addComponent(vbodaGradeLabel)
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(vbodaGradeField, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE))
+                                .addComponent(jLabel7)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(vbodaGradeField, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(24, 24, 24)
-                                .addComponent(notesLabel)
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(notesField, GroupLayout.PREFERRED_SIZE, 363, GroupLayout.PREFERRED_SIZE))
+                                .addComponent(jLabel8)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(notesField, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(24, 24, 24)
-                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(composerLabel)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(jLabel4)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                         .addComponent(composerField))
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(titleLabel)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(titleField, GroupLayout.PREFERRED_SIZE, 371, GroupLayout.PREFERRED_SIZE))))
-                            .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addComponent(jLabel3)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(titleField, javax.swing.GroupLayout.PREFERRED_SIZE, 371, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addGap(24, 24, 24)
-                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                                    .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                        .addComponent(arrangerLabel)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(arrangerField, GroupLayout.PREFERRED_SIZE, 345, GroupLayout.PREFERRED_SIZE))
-                                    .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                        .addComponent(publisherLabel)
-                                        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                                        .addComponent(publisherField, GroupLayout.PREFERRED_SIZE, 347, GroupLayout.PREFERRED_SIZE)))))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addComponent(jLabel5)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(arrangerField, javax.swing.GroupLayout.PREFERRED_SIZE, 345, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addComponent(jLabel6)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(publisherField, javax.swing.GroupLayout.PREFERRED_SIZE, 347, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                         .addGap(0, 40, Short.MAX_VALUE))
-                    .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(addButton)
                         .addGap(18, 18, 18)
@@ -184,36 +188,36 @@ public class EditFrame extends JPanel
                 .addContainerGap())
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(34, 34, 34)
                 .addComponent(directions)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(titleLabel)
-                    .addComponent(titleField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(titleField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(composerLabel)
-                    .addComponent(composerField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(composerField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(arrangerLabel)
-                    .addComponent(arrangerField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(arrangerField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(publisherLabel)
-                    .addComponent(publisherField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel6)
+                    .addComponent(publisherField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                    .addComponent(vbodaGradeLabel)
-                    .addComponent(vbodaGradeField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel7)
+                    .addComponent(vbodaGradeField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addComponent(notesLabel)
-                    .addComponent(notesField, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel8)
+                    .addComponent(notesField, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(addButton)
                     .addComponent(backButton))
                 .addContainerGap())
@@ -244,8 +248,8 @@ public class EditFrame extends JPanel
         // TODO add your handling code here:
     }//GEN-LAST:event_notesFieldActionPerformed
 
-    private void addButtonActionPerformed(ActionEvent evt)
-    {
+    private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
+        // TODO add your handling code here:
         Composition c = new Composition(titleField.getText(),
                                             composerField.getText(),
                                             arrangerField.getText(),
@@ -253,5 +257,12 @@ public class EditFrame extends JPanel
                                             Integer.parseInt(vbodaGradeField.getText()),
                                             notesField.getText());
         Library.append(c);
+    }//GEN-LAST:event_addButtonActionPerformed
+
+    public static void main(String[] args)
+    {
+        EditFrame frame = new EditFrame();
+
+        frame.setVisible(true);
     }
 }
